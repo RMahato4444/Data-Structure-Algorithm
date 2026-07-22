9a3f98259401511955b41b033326970135a9ae02
Rahul Mahato <rahulm4080@gmail.com>
2026-07-22 22:35:25 +0530

    fibonacci number with memoization and tabulation

---
+++ Dynamic Programming/fibonacci_tabulation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int prev2 = 0, prev = 1;
+    for(int i = 2; i<= n; i++){
+      int curi = prev2 + prev;
+      prev2 = prev;
+      prev = curi;
+    }
+    cout<<prev;
+    return 0;
+}
